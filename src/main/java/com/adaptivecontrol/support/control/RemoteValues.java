8c3904a67478ca1a94254b76ebdae23e5b0b862a
Adaptive <davidtindale@ntlworld.com>
2019-05-30 14:36:58 +0100

    Fixed the signature of RemoteValues

--- src/main/java/com/adaptivecontrol/support/control/RemoteValues.java
+++ src/main/java/com/adaptivecontrol/support/control/RemoteValues.java
@@ -1,23 +1,25 @@
 package com.adaptivecontrol.support.control;
 
+import org.jetbrains.annotations.Nullable;
+
 public interface RemoteValues {
-//    /**
-//     * Sets values for properties of given names.
-//     * For example:
-//     *     SetValues("Var1", 46, "Var2", "Abc")
-//     * Performed asynchronously, so it is not possible to tell whether the method succeeded.
-//     */
-//    fun setValues(vararg namesAndValues: Any?)
-//
-//    /**
-//     * Gets the latest values of a property.
-//     * 'name' is the zero-based index of the property, based on the order the properties were named in the call to CreateRemoteValues.
-//     */
-//    fun value(index: Int): Any?
-//
-//    /**
-//     * Gets whether the last refresh of the values went ok.
-//     * As long as the values have been refreshed successfully at least once, then it is ok to call 'Value'.
-//     */
-//    val lastOK: Boolean
+    /**
+     * Sets values for properties of given names.
+     * For example:
+     *     setValues("Var1", 46, "Var2", "Abc")
+     * Performed asynchronously, so it is not possible to tell whether the method succeeded.
+     */
+    void setValues(@Nullable Object ...namesAndValues);
+
+    /**
+     * Gets the latest values of a property.
+     * 'name' is the zero-based index of the property, based on the order the properties were named in the call to CreateRemoteValues.
+     */
+    @Nullable Object value(int index);
+
+    /**
+     * Gets whether the last refresh of the values went ok.
+     * As long as the values have been refreshed successfully at least once, then it is ok to call 'Value'.
+     */
+    boolean getLastOK();
 }
