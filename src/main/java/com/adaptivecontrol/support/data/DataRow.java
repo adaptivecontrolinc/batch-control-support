8a1387d259fd1106904fd6ab1f90461b0f57affa
Adaptive <davidtindale@ntlworld.com>
2019-06-01 13:33:28 +0100

    Fixed the signature of dbGetDataTable

--- src/main/java/com/adaptivecontrol/support/data/DataRow.java
+++ src/main/java/com/adaptivecontrol/support/data/DataRow.java
@@ -1,7 +1,9 @@
 package com.adaptivecontrol.support.data;
 
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public interface DataRow {
     @Nullable Object getItem(int columnIndex);
+    @Nullable Object getItem(@NotNull String columnName);
 }
